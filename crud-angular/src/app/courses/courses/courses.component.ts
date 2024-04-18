89e163e329150b4c7433e07b964aff9184d9e557
Arthur <nedochetkoarthur1@gmail.com>
2024-04-18 10:16:37 -0300

    first commit

---
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -0,0 +1,38 @@
+import { CoursesService } from './../services/courses.service';
+import { Component, OnInit } from '@angular/core';
+import { Course } from '../model/course';
+import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-courses',
+  templateUrl: './courses.component.html',
+  styleUrls: ['./courses.component.scss']
+})
+export class CoursesComponent implements OnInit {
+
+  courses$: Observable<Course[]>;
+  
+  displayedColumns = ['name','category'];
+
+  
+
+  constructor(private coursesService: CoursesService) {
+    
+    
+    this.courses$ = this.coursesService.list()
+    .pipe(
+      catchError(error => {
+        console.log(error)
+        return of([])
+      })
+    );
+
+  
+  }
+
+  ngOnInit(): void {
+
+  }
+
+}
